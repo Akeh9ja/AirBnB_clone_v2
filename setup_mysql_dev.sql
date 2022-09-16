d52f2e444e66680e805382ad23ad9ae4faae4d21
Akeh9ja <wilfreddbest1@gmail.com>
2022-09-16 10:07:16 +0100

    modified and added more functionalities

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,9 @@
---Setup Development for MySQL Server--
+-- setup development database in MySQL server
+-- create dababase
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create a user and grant all privilledges
--- with password 'hbnb_dev_pwd'
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev_db'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant select privilledges on performance_schema
+-- create user hbnb_dev 
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant priviledges on hbnb_dev_db
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+--Grant select priviledges on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
